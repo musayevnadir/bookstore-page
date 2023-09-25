c11171b459a9f278947c8df96a81b49f285016cf
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-09-25 17:07:45 +0400

    Home page changes

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-const footer = document.querySelector(".footer-container");
-const footerP = document.querySelector(".terms");
+const popup = document.querySelector(".popup");
+const showPopupIcon = document.querySelector(".showPopupIcon");
+const showPopupButton = document.querySelector(".showPopupBtn");
+const closePopupButton = document.getElementById("closePopup");
 const header = document.querySelector("header");
 
-// Store the original content of footer, footerP, and header
-const originalFooterContent = footer.innerHTML;
-const originalFooterPContent = footerP.innerHTML;
-const originalHeaderContent = header.innerHTML;
+// Function to open the pop-up
+function openPopup() {
+    popup.style.display = "block";
+    header.style.zIndex = "0";
+}
 
-window.addEventListener("resize", () => {
-    if (window.innerWidth < 650) {
-        footer.innerHTML = `
-            <div class="wrapper">
-            <div class="header-container" style="margin: 0 auto; display: flex; flex-direction: column;">
-                <header>
-                    <img src="/images/Лого.svg" alt="Logo" />
-                    <!-- Navigation -->
-                    <nav>
-                        <ul>
-                            <a href="#"><li>Home</li></a>
-                            <a href="#"><li>Catalog</li></a>
-                            <a href="#"><li>About Store</li></a>
-                            <a href="#"><li>Contact Us</li></a>
-                            <a href="#"><li>Search</li></a>
-                        </ul>
-                    </nav>
+// Function to close the pop-up
+function closePopup() {
+    popup.style.display = "none";
+}
 
-                    <hr/>
-                    <p>Library Bookstore EACamp</p>
-                    <p>© 2003-2023</p>
-                </header>
-            </div>
-            </div>
-        `;
-        footerP.innerHTML = "";
-        header.innerHTML = ""; // Clear the header content
-    } else {
-        // Restore the original content
-        footer.innerHTML = originalFooterContent;
-        footerP.innerHTML = originalFooterPContent;
-        header.innerHTML = originalHeaderContent;
+// Event listeners
+showPopupIcon.addEventListener("click", openPopup);
+showPopupButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+
+// Close the pop-up if the user clicks outside of it
+window.addEventListener("click", function (event) {
+    if (event.target === popup) {
+        closePopup();
     }
 });
