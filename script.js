07f737ecd88d1276856f13666282e40edb88e710
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-09-26 17:48:44 +0400

    End of the home page

--- script.js
+++ script.js
@@ -3,26 +3,54 @@ const showPopupIcon = document.querySelector(".showPopupIcon");
 const showPopupButton = document.querySelector(".showPopupBtn");
 const closePopupButton = document.getElementById("closePopup");
 const header = document.querySelector("header");
+const headerInfo = document.querySelector(".header-container");
+const sectionInfo = document.querySelector(".section-js");
+const input = document.querySelector(".hamburger");
+const style = document.createElement("style");
 
-// Function to open the pop-up
 function openPopup() {
     popup.style.display = "block";
     header.style.zIndex = "0";
 }
 
-// Function to close the pop-up
 function closePopup() {
     popup.style.display = "none";
 }
 
-// Event listeners
 showPopupIcon.addEventListener("click", openPopup);
 showPopupButton.addEventListener("click", openPopup);
 closePopupButton.addEventListener("click", closePopup);
 
-// Close the pop-up if the user clicks outside of it
-window.addEventListener("click", function (event) {
+window.addEventListener("click", (event) => {
     if (event.target === popup) {
         closePopup();
     }
 });
+
+// Hamburger menu
+const hamburger = document.getElementById("checkbox");
+const sideBar = document.querySelector(".burger header nav ul ");
+const wrapper = document.querySelector(".wrapper");
+
+hamburger.addEventListener("click", () => {
+    if (hamburger.checked) {
+        sideBar.style.display = "flex";
+        sideBar.style.zIndex = "2";
+        sideBar.style.alignItems = "flex-end";
+        wrapper.style.background = "rgba(36, 20, 0, 0.90)";
+        header.style.background = "rgba(36, 20, 0, 0.90)";
+        sectionInfo.style.marginTop = "0";
+        const style = document.createElement("style");
+        style.innerHTML = `
+  .hamburger::before {
+    background-color: red; /* Replace with your desired color */
+  }
+`;
+    } else {
+        sideBar.style.display = "none";
+        wrapper.style.background = "none";
+        header.style.background = "none";
+        sectionInfo.style.marginTop = "5rem";
+    }
+});
+document.head.append(style);
