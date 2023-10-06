0b1719462861f43646184dfeb346ce825b7a6881
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-10-06 09:30:41 +0400

    commit

--- catalog/catalog.js
+++ catalog/catalog.js
@@ -1,70 +1,70 @@
 /** @format */
 const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: false,
-  speed: 800,
-  slidesPerView: 5,
-  slidesPerGroup: 1,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-
-  //   spaceBetween:10
-
-  //   keyboard: {
-  //     enabled: true,
-  //     onlyInViewport: true,
-  //   },
-
-  //   ?--------------------
-  //     simulateTouch: true,
-  //     touchRatio: 1,
-  //     touchAngle: 45,
-  //     grabCursor: true,
-  //   slideToClickedlide: true,
-  //   ?--------------------
-
-  //   ! RESPONSE Swipper
-
-  breakpoints: {
-    375: {
-      slidesPerView: 1,
+    direction: "horizontal",
+    loop: false,
+    speed: 800,
+    slidesPerView: 5,
+    slidesPerGroup: 1,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    558: {
-      slidesPerView: 2,
+    scrollbar: {
+        el: ".swiper-scrollbar",
     },
-    992: {
-      slidesPerView: 5,
+
+    //   spaceBetween:10
+
+    //   keyboard: {
+    //     enabled: true,
+    //     onlyInViewport: true,
+    //   },
+
+    //   ?--------------------
+    //     simulateTouch: true,
+    //     touchRatio: 1,
+    //     touchAngle: 45,
+    //     grabCursor: true,
+    //   slideToClickedlide: true,
+    //   ?--------------------
+
+    //   ! RESPONSE Swipper
+
+    breakpoints: {
+        375: {
+            slidesPerView: 1,
+        },
+        558: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 5,
+        },
     },
-  },
 });
 
 // Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 import {
-  getDatabase,
-  ref,
-  get,
-  set,
-  onValue,
-  child,
-  push,
+    getDatabase,
+    ref,
+    get,
+    set,
+    onValue,
+    child,
+    push,
 } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOPLmzbWpJJqh3v9jxPilDKFrJlgiZi6E",
-  authDomain: "bookstore-3597f.firebaseapp.com",
-  databaseURL:
-    "https://bookstore-3597f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "bookstore-3597f",
-  storageBucket: "bookstore-3597f.appspot.com",
-  messagingSenderId: "659320410615",
-  appId: "1:659320410615:web:0c78bc27aef1b9c2b65ef2",
+    apiKey: "AIzaSyCOPLmzbWpJJqh3v9jxPilDKFrJlgiZi6E",
+    authDomain: "bookstore-3597f.firebaseapp.com",
+    databaseURL:
+        "https://bookstore-3597f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bookstore-3597f",
+    storageBucket: "bookstore-3597f.appspot.com",
+    messagingSenderId: "659320410615",
+    appId: "1:659320410615:web:0c78bc27aef1b9c2b65ef2",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -78,23 +78,21 @@ const biographyBooks = [];
 const dramaBooks = [];
 
 onValue(bookDataRef, (snapshot) => {
-<<<<<<< HEAD
-  const bookDatas = snapshot.val();
-  for (const key in bookDatas) {
-    const swiperSlideContainer = document.createElement("div");
-    swiperSlideContainer.classList.add("swiper-slide");
-    swiperSlideContainer.classList.add("slide-contect-container");
-    swiperSlideContainer.classList.add("all-books");
-=======
     const bookDatas = snapshot.val();
-
     for (const key in bookDatas) {
-        allBooks.push(bookDatas[key]);
         const swiperSlideContainer = document.createElement("div");
         swiperSlideContainer.classList.add("swiper-slide");
         swiperSlideContainer.classList.add("slide-contect-container");
+        swiperSlideContainer.classList.add("all-books");
+        const bookDatas = snapshot.val();
 
-    swiperSlideContainer.innerHTML = `
+        for (const key in bookDatas) {
+            allBooks.push(bookDatas[key]);
+            const swiperSlideContainer = document.createElement("div");
+            swiperSlideContainer.classList.add("swiper-slide");
+            swiperSlideContainer.classList.add("slide-contect-container");
+
+            swiperSlideContainer.innerHTML = `
         <div class='book-img-container'>
             <img src="${bookDatas[key].image}" />
             <h4>${bookDatas[key].name}</h4>
@@ -105,53 +103,50 @@ onValue(bookDataRef, (snapshot) => {
         </div>
         
         `;
+            swiperContainer.append(swiperSlideContainer);
+            document
+                .getElementById(`${bookDatas[key].id}`)
+                .addEventListener("click", () => {
+                    window.location.href = "https://google.com";
+                });
+            // if (bookDatas[key].type === "Fiction") {
+            //     finctionBooks.push(bookDatas[key]);
+            //     for (let i = 0; i < finctionBooks.length; i++) {
+            //         const swiperSlideCont = document.createElement("div");
+            //         swiperSlideCont.classList.add("swiper-slide");
+            //         swiperSlideCont.classList.add("slide-contect-container");
+            //         swiperSlideCont.classList.add("fiction-books");
+            //         swiperSlideCont.innerHTML = `
+            //             <div class='book-img-container'>
+            //                 <img src="${finctionBooks[i].image}" />
+            //                 <h4>${finctionBooks[i].name}</h4>
+            //                 <p>${finctionBooks[i].author}</p>
+            //                 <div class="btn-container">
+            //                     <button>Read more</button>
+            //                 </div>
+            //             </div>
 
-<<<<<<< HEAD
-    swiperContainer.append(swiperSlideContainer);
-    document
-      .getElementById(`${bookDatas[key].id}`)
-      .addEventListener("click", () => {
-        window.location.href = "https://google.com";
-      });
-    // if (bookDatas[key].type === "Fiction") {
-    //     finctionBooks.push(bookDatas[key]);
-    //     for (let i = 0; i < finctionBooks.length; i++) {
-    //         const swiperSlideCont = document.createElement("div");
-    //         swiperSlideCont.classList.add("swiper-slide");
-    //         swiperSlideCont.classList.add("slide-contect-container");
-    //         swiperSlideCont.classList.add("fiction-books");
-    //         swiperSlideCont.innerHTML = `
-    //             <div class='book-img-container'>
-    //                 <img src="${finctionBooks[i].image}" />
-    //                 <h4>${finctionBooks[i].name}</h4>
-    //                 <p>${finctionBooks[i].author}</p>
-    //                 <div class="btn-container">
-    //                     <button>Read more</button>
-    //                 </div>
-    //             </div>
+            //             `;
+            //         const allBooksElements =
+            //             document.querySelectorAll(".all-books");
+            //         const fictionBooksElements =
+            //             document.querySelectorAll(".fiction-books");
 
-    //             `;
-    //         const allBooksElements =
-    //             document.querySelectorAll(".all-books");
-    //         const fictionBooksElements =
-    //             document.querySelectorAll(".fiction-books");
+            //         allBooksElements.forEach((element) => {
+            //             element.style.display = "none";
+            //         });
 
-    //         allBooksElements.forEach((element) => {
-    //             element.style.display = "none";
-    //         });
-
-    //         fictionBooksElements.forEach((element) => {
-    //             element.style.display = "block";
-    //         });
-    //         swiperContainer.append(swiperSlideCont);
-    //     }
-    // }
-    // const bookImgContainer = document.createElement("div");
-    // bookImgContainer.classList.add("book-img-container");
-    // const btnContainer = document.createElement("div");
-    // btnContainer.classList.add("btn-container");
-  }
-=======
+            //         fictionBooksElements.forEach((element) => {
+            //             element.style.display = "block";
+            //         });
+            //         swiperContainer.append(swiperSlideCont);
+            //     }
+            // }
+            // const bookImgContainer = document.createElement("div");
+            // bookImgContainer.classList.add("book-img-container");
+            // const btnContainer = document.createElement("div");
+            // btnContainer.classList.add("btn-container");
+        }
         swiperContainer.append(swiperSlideContainer);
         document
             .getElementById(`${bookDatas[key].id}`)
@@ -312,5 +307,4 @@ document.getElementById("biography").addEventListener("click", () => {
         `;
         swiperContainer.append(swiperSlide);
     }
->>>>>>> origin
 });
